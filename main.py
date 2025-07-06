d87d2596e71dede3828c391eb0fb70f3bc21cdd5
sgelhaus <10716580+sgelhaus@users.noreply.github.com>
2025-07-06 14:00:15 -0400

    Adds drop down list

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 import json
 import streamlit as st
 import pandas as pd
+from pandas import json_normalize
 
 url = 'https://randomuser.me/api/'
 headers = {
@@ -10,38 +11,72 @@ headers = {
 }
 session = Session()
 session.headers.update(headers)
+responseList = []
 try:
-    response = session.get(url)
+    response = session.get(url, params={'results': 5})
     dataObj = json.loads(response.text)
     json_formatted_str = json.dumps(dataObj, indent=4)
+    responseList.append(dataObj)
     print(json_formatted_str)
     print(dataObj['results'][0]['name'])
 except (ConnectionError, Timeout, TooManyRedirects) as e:
     print(e)
 
+
+prefix = dataObj['results']
+
+df = json_normalize(prefix)
+
+# Create a column to identify users by name
+df['fullName'] = df['name.first'] + ' ' + df['name.last']
+
+# Dropdown list
+selected_name = st.selectbox("Select a user to view details:", df['fullName'])
+
+# Find the row for the selected user
+selected_user = df[df['fullName'] == selected_name].iloc[0]
+
+
+firstEntry = lastEntry = None
+
+
+# Convert list to Data Frame
+df = pd.DataFrame(responseList)
+
+startData = []
+for num in range(5): 
+    firstEntry = prefix[num]['name']['first']
+    lastEntry = prefix[num]['name']['last']
+    listUser = {
+        'First': firstEntry,
+        'Last': lastEntry
+    }
+    startData.append(listUser)
+
+
 st.write("Personal Profile")
 nameData = []
 nameData.append(
     {
-        'First': dataObj['results'][0]['name']['first'],
-        'Last': dataObj['results'][0]['name']['last']
+        'First': firstEntry,
+        'Last': lastEntry
     }
 )
 contactData = []
 contactData.append(
     {
-        'Email': dataObj['results'][0]['email'],
-        'Phone': dataObj['results'][0]['phone'],
-        'Cell' : dataObj['results'][0]['cell']
+        'Email': prefix[0]['email'],
+        'Phone': prefix[0]['phone'],
+        'Cell' : prefix[0]['cell']
     }
 )
-# Picture at top
-st.image(dataObj['results'][0]['picture']['large'])
+
+# Profile picture 
+st.image(prefix[0]['picture']['large'])
 
 # Make Data Frame
 df = pd.DataFrame(nameData)
 dfContact = pd.DataFrame(contactData)
-
 
 # Create tabs
 tabName, tabContact = st.tabs(["Name", "Contact"])
